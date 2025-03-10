72f774b2a19c8349999ad732374c7e39c4c5861d
CodingBlow <mdfarhanraza001@gmail.com>
2025-03-11 00:11:19 +0530

    Updated

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Downloa from "../images/ImageHERE.png";
+import Downloa from "../images/last_Image.png";
 
 const ErrorPage = () => {
   const navigate = useNavigate();
